bbb8c91a10e5a6bcdc1fe4cfb15d59fa5f2c2b90
KonTroAl <troshenkin.k@yandex.ru>
2021-01-23 17:48:26 +0500

    create context for index.html
    title + year

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,7 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'geekShop',
+        'Year': '2021',
+    }
+    return render(request, 'mainapp/index.html', context)
+
 
 def products(request):
     return render(request, 'mainapp/products.html')
