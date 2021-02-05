7b8a0f2c125c150c33be192f63cb1c5412b832f2
KonTroAl <troshenkin.k@yandex.ru>
2021-02-05 14:44:05 +0500

    add login_required

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
 from mainapp.models import Product
 from basketapp.models import Basket
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -19,6 +21,7 @@ def basket_add(request, product_id):
         basket.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+@login_required
 def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
