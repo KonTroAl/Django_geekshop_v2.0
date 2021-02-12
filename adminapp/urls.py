6dd17d27ef1867797d177cbb61508f6e7d24e9f2
KonTroAl <troshenkin.k@yandex.ru>
2021-02-12 21:29:53 +0500

    added CBV for user admin

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,10 +6,10 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('admin_users_read/', views.admin_users_read, name='admin_users_read'),
-    path('admin_users_create/', views.admin_users_create, name='admin_users_create'),
-    path('admin_users_update/<int:id>/', views.admin_users_update, name='admin_users_update'),
-    path('admin_users_delete/<int:id>/', views.admin_users_delete, name='admin_users_delete'),
+    path('admin_users_read/', views.UserListView.as_view(), name='admin_users_read'),
+    path('admin_users_create/', views.UserCreateView.as_view(), name='admin_users_create'),
+    path('admin_users_update/<int:pk>/', views.UserUpdateView.as_view(), name='admin_users_update'),
+    path('admin_users_delete/<int:pk>/', views.UserDeleteView.as_view(), name='admin_users_delete'),
     path('admin_users_recover/<int:id>/', views.admin_users_recover, name='admin_users_recover'),
     path('admin_category_read/', views.admin_category_read, name='admin_category_read'),
     path('admin_category_create/', views.admin_category_create, name='admin_category_create'),
