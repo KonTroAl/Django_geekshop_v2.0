5e46f33a5a2608945491d5dc9d1183a0bd97f4fc
KonTroAl <troshenkin.k@yandex.ru>
2021-02-15 22:39:44 +0500

    update template products.html

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger, Page
 from django.views.generic.list import ListView
 
 from mainapp.models import ProductCategory, Product
