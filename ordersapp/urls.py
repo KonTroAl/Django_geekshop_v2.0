0e7fe4dceabcb9c6309ef2565441b8e3736757b8
KonTroAl <troshenkin.k@yandex.ru>
2021-03-20 15:39:24 +0500

    added all CBV for ordersapp

--- ordersapp/urls.py
+++ ordersapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from ordersapp import views
 
@@ -6,4 +6,9 @@ app_name = 'ordersapp'
 
 urlpatterns = [
     path('', views.OrderList.as_view(), name='order_list'),
+    path('forming/complete/<int:pk>/', views.order_forming_complete, name='order_forming_complete'),
+    path('create/', views.OrderItemsCreate.as_view(), name='order_create'),
+    path('read/<int:pk>/', views.OrderRead.as_view(), name='order_read'),
+    path('update/<int:pk>/', views.OrderItemsUpdate.as_view(), name='order_update'),
+    path('delete/<int:pk>/', views.OrderDelete.as_view(), name='order_delete'),
 ]
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.forms import inlineformset_factory
 from django.db import transaction
+from django.shortcuts import get_object_or_404, HttpResponseRedirect
 
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 
 from ordersapp.models import Order, OrderItem
 from ordersapp.forms import OrderForm, OrderItemForm
+
 
 # Create your views here.
 
@@ -15,6 +18,7 @@ class OrderList(ListView):
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
+
 
 class OrderItemsCreate(CreateView):
     model = Order
@@ -49,6 +53,7 @@ class OrderItemsCreate(CreateView):
 
         return super().form_valid(form)
 
+
 class OrderItemsUpdate(UpdateView):
     model = Order
     fields = []
@@ -79,3 +84,25 @@ class OrderItemsUpdate(UpdateView):
             self.object.delete()
 
         return super().form_valid(form)
+
+
+class OrderDelete(DeleteView):
+    model = Order
+    success_url = reverse_lazy('ordersapp:order_list')
+
+
+class OrderRead(DetailView):
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderRead, self).get_context_data(**kwargs)
+        context['title'] = 'order/view'
+        return context
+
+
+def order_forming_complete(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    order.status = Order.SENT_TO_PROCEED
+    order.save()
+
+    return HttpResponseRedirect(reverse('ordersapp:order_list'))
