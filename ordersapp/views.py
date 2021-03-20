8fece1d6dca6e4ad6e3080aedda7fea9533e6a61
KonTroAl <troshenkin.k@yandex.ru>
2021-03-20 14:47:45 +0500

    added OrderItemsUpdate

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.forms import inlineformset_factory
 from django.db import transaction
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from ordersapp.models import Order, OrderItem
 from ordersapp.forms import OrderForm, OrderItemForm
@@ -49,3 +49,33 @@ class OrderItemsCreate(CreateView):
 
         return super().form_valid(form)
 
+class OrderItemsUpdate(UpdateView):
+    model = Order
+    fields = []
+    success_url = reverse_lazy('ordersapp:order_list')
+
+    def get_context_data(self, **kwargs):
+        data = super(OrderItemsUpdate, self).get_context_data(**kwargs)
+        OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
+
+        if self.request.POST:
+            data['orderitems'] = OrderFormSet(self.request.POST, instance=self.object)
+        else:
+            data['orderitems'] = OrderFormSet(instance=self.object)
+        return data
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        orderitems = context['orderitems']
+
+        with transaction.atomic():
+            form.instance.user = self.request.user
+            self.object = form.save()
+            if orderitems.is_valid():
+                orderitems.instance = self.object
+                orderitems.save()
+
+        if self.object.get_total_cost() == 0:
+            self.object.delete()
+
+        return super().form_valid(form)
