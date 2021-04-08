9fa569df21b1a274bc78fc28690d37ef88ad0cc7
KonTroAl <troshenkin.k@yandex.ru>
2021-04-08 13:57:56 +0500

    update models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.conf import settings
 from django.core.cache import cache
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 # Create your models here.
 
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -37,6 +40,15 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.name} | {self.category.name}'
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_save_productcategory(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
